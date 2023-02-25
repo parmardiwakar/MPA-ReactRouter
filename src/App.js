7d9befcb361d96ad731e4e2bc8424ff40d84ab8e
Diwakar Parmar <diwakarparmar@gmail.com>
2023-02-25 15:56:52 -0500

    Added second route to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/products",
+    element: <ProductsPage />,
   },
 ]);
 
