e923e6e63c01337b1c77c514689bc31a4446952d
Diwakar Parmar <diwakarparmar@gmail.com>
2023-02-25 16:23:42 -0500

    Added navigation b/w pages with Link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
-
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div>Home</div>
+      <p>Go to <Link to="products">the list of products.</Link></p>
+    </>
+  );
 };
 
 export default Home;
