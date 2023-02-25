a485b25f6270410acc03c39e4cdc9ea3e3aa1241
Diwakar Parmar <diwakarparmar@gmail.com>
2023-02-25 16:44:03 -0500

    Added navigation and nested routes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/products",
-    element: <ProductsPage />,
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/products",
+        element: <ProductsPage />,
+      },
+    ],
   },
 ]);
 
